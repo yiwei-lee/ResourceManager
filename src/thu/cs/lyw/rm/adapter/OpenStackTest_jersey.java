c96a0e15fd642be47bb55616e0ae3bc45ba04f76
yiwei-lee <lywthu@yeah.net>
2013-04-22 15:32:24 +0800

    OpenStack tested.

--- src/thu/cs/lyw/rm/adapter/OpenStackTest_jersey.java
+++ src/thu/cs/lyw/rm/adapter/OpenStackTest_jersey.java
@@ -1,33 +1,37 @@
 package thu.cs.lyw.rm.adapter;
 
 import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
 public class OpenStackTest_jersey {
+	private static Client client  = Client.create();
+	private static WebResource webResource;
+	private static String computeHeader = null;
+	private static String token = null;
 	public static void main(String[] args) throws Exception {
 		try{
 			//First step : get token;
-			Client client = Client.create();
-			WebResource webResource = client
+			webResource = client
 					.resource("https://region-a.geo-1.identity.hpcloudsvc.com:35357/v2.0/tokens");
 			String input = "{\"auth\":{\"apiAccessKeyCredentials\":{\"accessKey\":" +
 					"\"D7WAWP2LBYLGRZ9FCLNE\",\"secretKey\":\"+fmjdLc4UBvn/gsfEYWnD5AcbOuhEsP30SbJRc/c\"}}}";
 			ClientResponse response = webResource.type("application/json").accept("application/json")
 					.post(ClientResponse.class, input);
-			checkResponseCode(response);
+			checkResponseCode(response, 200);
 			String output = response.getEntity(String.class);
 			JSONObject json = new JSONObject(output);
-			String token = json.getJSONObject("access").getJSONObject("token").getString("id");
+			token = json.getJSONObject("access").getJSONObject("token").getString("id");
 			
 			//Second step : get tenant ID;
 			webResource = client.resource("https://region-a.geo-1.identity.hpcloudsvc.com:35357/v2.0/tenants");
 			response = webResource.type("application/json").accept("application/json").header("X-Auth-Token", token)
 					.header("Conection", "keep-alive")
 					.get(ClientResponse.class);
-			checkResponseCode(response);
+			checkResponseCode(response, 200);
 			output = response.getEntity(String.class);
 			json = new JSONObject(output);
 			String tenantId = json.getJSONArray("tenants").getJSONObject(0).getString("id");
@@ -39,11 +43,11 @@ public class OpenStackTest_jersey {
 					"\""+tenantId+"\"}}";
 			response = webResource.type("application/json").accept("application/json")
 					.post(ClientResponse.class, input);
-			checkResponseCode(response);
+			checkResponseCode(response, 200);
 			output = response.getEntity(String.class);
 			json = new JSONObject(output);
+			token = json.getJSONObject("access").getJSONObject("token").getString("id");
 			JSONArray jsonArray = json.getJSONObject("access").getJSONArray("serviceCatalog");
-			String computeHeader = null;
 			for (int i = 0 ; i < jsonArray.length() ; i++){
 				json = jsonArray.getJSONObject(i);
 				if (!json.getString("type").equals("compute")) continue;
@@ -52,17 +56,55 @@ public class OpenStackTest_jersey {
 				}
 			}
 			if (computeHeader == null) System.err.println("Something's wrong!");
-			System.out.println(token);
-			System.out.println(tenantId);
-			System.out.println(computeHeader);
+			listFlavors();
+			listImages();
+			System.out.println("Tenant ID : " + tenantId);
+			System.out.println("Token : " + token);
+			//Final step : create a server;
+			webResource = client.resource(computeHeader + "/servers");
+			json = new JSONObject().put("server", new JSONObject().put("flavorRef", "100")
+					.put("imageRef", "81078").put("name", "server-no-1").put("min_count", "1")
+					.put("max_count", "1").put("key_name", "RM.OpenStack.Test"));
+			response = webResource.type("application/json").accept("application/json").header("X-Auth-Token", token)
+					.post(ClientResponse.class, json);
+			output = response.getEntity(String.class);
+			json = new JSONObject(output);
+			String serverId = json.getJSONObject("server").getString("id");
+			System.out.println("Server ID : " + serverId);
+			//So...time to kill!
+			webResource = client.resource(computeHeader + "/servers/"+serverId);
+			response = webResource.type("application/json").accept("application/json").header("X-Auth-Token", token)
+					.delete(ClientResponse.class);
+			checkResponseCode(response, 204);
+			System.out.println("Server deleted.");
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	private static void checkResponseCode(ClientResponse response){
-		if (response.getStatus() != 200) {
+	private static void checkResponseCode(ClientResponse response, int expectCode){
+		if (response.getStatus() != expectCode) {
 			throw new RuntimeException("Failed : HTTP error code : "
 			     + response.getStatus());
 		}
+	}
+	private static void listFlavors() throws JSONException{
+		webResource = client.resource(computeHeader + "/flavors/detail");
+		ClientResponse response = webResource.type("application/json").accept("application/json").header("X-Auth-Token", token)
+				.get(ClientResponse.class);
+		String output = response.getEntity(String.class);
+		JSONObject json = new JSONObject(output);
+//		System.out.println(json);
+		JSONArray array = json.getJSONArray("flavors");
+		System.out.println(array.length() + " flavors found.");
+	}
+	private static void listImages() throws JSONException{
+		webResource = client.resource(computeHeader + "/images");
+		ClientResponse response = webResource.type("application/json").accept("application/json").header("X-Auth-Token", token)
+				.get(ClientResponse.class);
+		String output = response.getEntity(String.class);
+		JSONObject json = new JSONObject(output);
+//		System.out.println(json);
+		JSONArray array = json.getJSONArray("images");
+		System.out.println(array.length() + " images found.");
 	}
 }
